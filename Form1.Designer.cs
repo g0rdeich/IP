dc5ce4aa5652cdb3c209d63daa5997d3e2d2c7d2
Alex Gordeev <gordeeffff@gmail.com>
2020-10-06 11:58:46 +0300

    push IP-check to git

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace IP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 287);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(40, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(40, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 163);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(401, 50);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(546, 408);
+            this.gMapControl1.TabIndex = 3;
+            this.gMapControl1.Visible = false;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(51, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 55);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите IP-адрес в формате (x.x.x.x)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(989, 492);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            gMapControl1.Bearing = 0;
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.GrayScaleMode = true;
+            gMapControl1.MaxZoom = 18;
+            gMapControl1.MinZoom = 2;
+            gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            gMapControl1.NegativeMode = false;
+            gMapControl1.PolygonsEnabled = true;
+            gMapControl1.ShowTileGridLines = false;
+            gMapControl1.Zoom = 2;
+            //gMapControl1.Dock = DockStyle.Fill;
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GMapProviders.GoogleMap;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line;
+            using (WebClient wc = new WebClient())
+                line = wc.DownloadString($"http://free.ipwhois.io/xml/{textBox1.Text}");
+            Match match = Regex.Match(line, "<country>(.*?)</country>(.*?)<latitude>(.*?)</latitude>(.*?)<longitude>(.*?)</longitude>");
+            Match match1 = Regex.Match(line, "<region>(.*?)</region>(.*?)<city>(.*?)</city>");
+            //"(.*?)<region>(.*?)</region>(.*?)<city>(.*?)</city>");
+            string country = match.Groups[1].Value;
+            string lat = match.Groups[3].Value.Replace(".",",");
+            string lon = match.Groups[5].Value.Replace(".", ",");
+            string reg = match1.Groups[1].Value;
+            string city = match1.Groups[3].Value;
+            label1.Text = country + "\n" + reg + "\n" + city;
+
+            double latd = Convert.ToDouble(lat);
+            double lond = Convert.ToDouble(lon);
+
+            gMapControl1.Visible = true;
+
+            gMapControl1.Position = new GMap.NET.PointLatLng(latd, lond);
+            gMapControl1.Zoom = 10;
+
+        }
+    }
+}
